e9c87555d3d96bb00821b093f4efa5ff36133810
Jesse Esau <jesseesau13@gmail.com>
2022-09-08 08:40:53 -0700

    4-puts.c, fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <studio.h>
 /**
- * main - Entry point
- * Return: 0 Success
+ * main - Prints "programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
-
-{puts("\" programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
