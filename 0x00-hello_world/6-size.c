36671fc244e74c38cdab642675a173d1707514c9
Jesse Esau <jesseesau13@gmail.com>
2022-09-08 09:58:04 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ float f;
 printf("Size of char:%d byte(s)\n", sizeof(a));
 	printf("Size of int:%d byte(s)\n", sizeof(b));
 	printf("Size of a long int:%d byte(s)\n", sizeof(c));
-	printf("Size of long long in:%d byte(s)\n", sizeof(d));
+	printf("Size of long long int:%d byte(s)\n", sizeof(d));
 	printf("Size of a float:%d byte(s)\n", sizeof(f));
 	return (0);
 }
